258a13d583469d85ac86190dd4a26f3e6c740848
Keymiuz <jpcicolo@gmail.com>
2025-03-24 02:36:50 -0300

    Commit inicial dos arquivos

---
+++ dia_02.py
@@ -0,0 +1,20 @@
+# %%
+import pandas as pd
+import os
+
+df = pd.read_csv('Data/clientes.csv')
+df
+
+
+# %%
+
+df.to_csv('Data/clientes.csv', index=False)
+# %%
+
+df.to_parquet('Data/clientes.parquet', index=False)
+
+# %%
+
+df.to_excel('Data/clientes.xlsx', index=False)
+
+# %%
